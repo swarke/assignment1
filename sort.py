d93ef05567300b59eec7d48ac559b99f9eb5f776
virajindasrao <indasraoviraj1512@gmail.com>
2016-04-12 23:24:02 +0530

    Added files via upload

--- sort.py
+++ sort.py
@@ -5,20 +5,27 @@ def sortString(sortBy):
         array = []
         for line in ins:
             array.append(line)
-
-    
     array.sort(reverse=sortBy)
-    for element in array:
-        print(element)
-
+    return array
+ 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option('-n', dest='num', type='int')
+    parser.add_option('-n', dest='num', type='int', help='Enter 0 or 1 to sort array desc or asc')
+    parser.add_option('-o', dest='out', type='string', help='Specify output file')
     (options,args) = parser.parse_args()
+
     if (options.num > 0):
-        sortString(True)
+        result = sortString(True)
     else:
-        sortString(False)
+        result = sortString(False)
+
+    for i in result:
+        print(i)
+        if(options.out != None and options.out!='sort.txt'):
+            f = open(options.out, 'a')
+            f.write(str(result)+'\n')
+
+    
 
 if __name__ == '__main__' :
     main()
